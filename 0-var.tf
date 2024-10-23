7b1ed7d3e176ccc5b370861c4d43bea5a0f77615
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-10-22 18:47:13 -0700

    basic setup

---
+++ 0-var.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+
+
+variable "vpc1_cidr" {
+  default = "10.10.0.0/16"
+}
+
+
+
+
+variable "public_subnet_cidr" {
+  default = "10.10.1.0/24"
+}
+
+
+
+variable "us-east-1-linux" {
+  default = "ami-06b21ccaeff8cd686"
+}
+
+variable "ec2-key" {
+  default = "tf-key-test"
+}
+
